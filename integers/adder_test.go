9f0d40f365d794441a2ed357381fdef884ea9591
Darshit Shah <ddshah24595@gmail.com>
2022-01-02 12:00:24 +0530

    Added Integers

---
+++ integers/adder_test.go
@@ -0,0 +1,14 @@
+package integers
+
+import "testing"
+
+func TestAdderFunc (t *testing.T) {
+	t.Run("Regular Sum", func (t *testing.T) {
+		want := 42
+		got := Add(40, 2)
+
+		if want != got {
+			t.Errorf("got: %d, want: %d", got, want)
+		}
+	})
+}
