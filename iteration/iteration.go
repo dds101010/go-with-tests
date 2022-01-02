74729cf2ad4f5b3ba91066c1fb8c0b97cbaaa95a
Darshit Shah <ddshah24595@gmail.com>
2022-01-02 12:19:46 +0530

    Optimization for iteration

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,16 @@
 package iteration
 
+import "strings"
+
 func Repeat(text string, itr int) string {
 	var retVal string
 	for i := 0; i < itr; i++ {
 		retVal += text
 	}
 	return retVal
+}
+
+// Optimized version of Repeat function that uses lib implementation
+func RepeatOpt(text string, itr int) string {
+	return strings.Repeat(text, itr)
 }
