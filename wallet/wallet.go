57e48cd6580ec884969241b0b184235c404d9f14
Darshit Shah <ddshah24595@gmail.com>
2022-01-10 19:45:28 +0530

    Error handling

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,6 +1,9 @@
 package wallet
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -24,6 +27,18 @@ type Wallet struct {
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+// The var keyword allows us to define values global to the package.
+var ErrInsufficientFunds = errors.New("Insufficient balance")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -4,18 +4,47 @@ import (
 	"testing"
 )
 
-func TestWallet(t *testing.T) {
-
-	wallet := Wallet{}
-
-	wallet.Deposit(Bitcoin(10))
-
-	// fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
 	got := wallet.Balance()
-	want := Bitcoin(10)
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
 	}
+}
+
+func assertError(t testing.TB, err error, expectedError error) {
+	t.Helper()
+	if err == nil {
+		// t.Fatal will stop the test if it is called
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if err != expectedError {
+		// %q will quote the value
+		t.Errorf("got %q want %q", err.Error(), expectedError)
+	}
+}
+
+func TestWallet(t *testing.T) {
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
+	})
 }
