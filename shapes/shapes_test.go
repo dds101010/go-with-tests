4837263d702c95e8ac5e4cbe83b37821bcaa23cf
Darshit Shah <ddshah24595@gmail.com>
2022-01-10 18:54:14 +0530

    Refactor with struct

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -3,7 +3,8 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -13,10 +14,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 20.0)
-	want := 200.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 20.0}
+		got := rectangle.Area()
+		want := 200.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			// Use of g will print a more precise decimal number in the error message
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
